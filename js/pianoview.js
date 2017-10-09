ec09941167230c2e9df9eb53b11fc3f881e1bdf3
Nebojsa Petrovic <nebspetrovic@gmail.com>
2017-10-09 13:53:12 -0400

    Update piano design

--- js/pianoview.js
+++ js/pianoview.js
@@ -15,28 +15,38 @@ class PianoView {
 			return;
 		}
 		
+		const backgroundColor = '#000';
+		const keyboardContainerTopPadding = 35;
+		const keyboardContainerBottomPadding = 5;
+		const keyboardContainerSidePadding = 8;
+		const keyboardContainerX = keyboardContainerSidePadding;
+		const keyboardContainerY = keyboardContainerTopPadding;
+		const keyboardContainerWidth = this.width - (keyboardContainerSidePadding * 2);
+		const keyboardContainerHeight = this.height - (keyboardContainerTopPadding + keyboardContainerBottomPadding);
 		const totalOctaves = Config.totalOctaves;
 		const notesPerOctave = Config.notesPerOctave;
-		
-		var ctx = this.$canvas.getContext('2d');
 		const whiteNoteIndexes = [1,3,5,6,8,10,12];
 		const blackNoteIndexes = [2,4,7,9,11];
 		const whiteNotesPerOctaveCount = 7;			
 		const blackNotesPerOctaveCount = 5;
 		const whiteNoteCount = totalOctaves * whiteNotesPerOctaveCount;
 		const blackNoteCount = totalOctaves * blackNotesPerOctaveCount;
-		const whiteNoteWidth = this.width / whiteNoteCount;
+		const whiteNoteWidth = keyboardContainerWidth / whiteNoteCount;
 		const blackNoteWidth = whiteNoteWidth * 0.5;
-		const whiteNoteHeight = this.height;
+		const whiteNoteHeight = keyboardContainerHeight;
 		const blackNoteHeight = whiteNoteHeight * 0.7;
+
+		var ctx = this.$canvas.getContext('2d');
+		ctx.fillStyle = backgroundColor;
+		ctx.fillRect(0, 0, this.width, this.height);
 
 		var i = 0;
 		var currentOctave = 0;		
 		for (i=0; i<whiteNoteCount; i++) {
 			currentOctave = Math.floor(i / whiteNotesPerOctaveCount);
-			const x = i * whiteNoteWidth;
-			const y = 0;
-			const w = whiteNoteWidth;
+			const x = i * whiteNoteWidth + keyboardContainerX + 1;
+			const y = keyboardContainerY;
+			const w = whiteNoteWidth - 2;
 			const h = whiteNoteHeight;
 			const noteIndex = whiteNoteIndexes[i % whiteNoteIndexes.length];
 			const indexToCheck = noteIndex + (currentOctave * notesPerOctave);
@@ -45,28 +55,25 @@ class PianoView {
 			} else {
 				ctx.fillStyle = '#FFF';
 			}				
-			ctx.strokeStyle = '#000';
-			ctx.lineWidth = 1;				
-			ctx.fillRect(x, y, w, h);				
-			ctx.strokeRect(x, y, w, h);	
+			ctx.fillRect(x, y, w, h);
 
 			if (this.settings.isNamesOn) {
 				var note_letters = ['C','D','E','F','G','A','B'];
 				ctx.font = '8px sans-serif';
 				ctx.fillStyle = '#999';
-				ctx.fillText(note_letters[i % note_letters.length], 4 + x, 46);					
+				ctx.fillText(note_letters[i % note_letters.length], 3 + x, y + h - 3);
 			}
 		}
 
 		const blackNoteOffsets = [1, 1, 2, 2, 2];
 		currentOctave = 0;
-		const octaveWidth = this.width / totalOctaves;
+		const octaveWidth = keyboardContainerWidth / totalOctaves;
 		for (i=0; i<blackNoteCount; i++) {
 			currentOctave = Math.floor(i / blackNotesPerOctaveCount);
 			const offsetIndex = i % blackNoteOffsets.length;
 			const offset = blackNoteOffsets[offsetIndex];
-			const x = (currentOctave * octaveWidth) + ((offsetIndex + offset) * whiteNoteWidth) - (blackNoteWidth / 2);
-			const y = 0;
+			const x = (currentOctave * octaveWidth) + ((offsetIndex + offset) * whiteNoteWidth) - (blackNoteWidth / 2) + keyboardContainerX;
+			const y = keyboardContainerY;
 			const w = blackNoteWidth;
 			const h = blackNoteHeight;
 			const noteIndex = blackNoteIndexes[i % blackNoteIndexes.length];
@@ -74,11 +81,15 @@ class PianoView {
 				ctx.fillStyle = '#A8B272';			
 			} else {
 				ctx.fillStyle = '#333';
-			}				
-			ctx.strokeStyle = '#000';
-			ctx.lineWidth = 1;
+			}
 			ctx.fillRect(x, y, w, h);				
-			ctx.strokeRect(x, y, w, h);
 		}
+		
+		ctx.fillStyle = '#E30035';
+		ctx.fillRect(keyboardContainerX, keyboardContainerY, keyboardContainerWidth, 1);
+		ctx.font = '25px Grand Hotel';
+		ctx.textAlign = 'center';
+		ctx.fillStyle = '#FFF';
+		ctx.fillText('note kitchen', this.width / 2, 26);		
 	} 
 }
