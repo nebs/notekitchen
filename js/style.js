633936e1a217b5ab28cc9515e7dd891701228c6f
Nebojsa Petrovic <nebspetrovic@gmail.com>
2017-10-12 10:03:47 -0400

    Darken piano shadow

--- js/style.js
+++ js/style.js
@@ -2,7 +2,7 @@ class Style {
 	static get pianoBackgroundColor() { return '#db7a7a'; }
     static get pianoHighlightColor() { return '#e58a8a'; }
     static get pianoHighlightSpeckColor() { return '#e8c5c5'; }
-    static get pianoShadowColor() { return '#c65353'; }
+    static get pianoShadowColor() { return '#b23b3b'; }
 	static get pianoDarkTextColor() { return '#888'; }
 	static get pianoLightTextColor() { return '#DDD'; }
 	static get pianoNoteSelectedColor() { return '#D80152'; }
